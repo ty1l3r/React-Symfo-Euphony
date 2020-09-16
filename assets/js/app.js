509780376c7d49d1199f0ac7866005c037941c8a
ty1l3r <ak-vitrail@live.fr>
2020-09-16 15:16:41 +0200

    Styles adjustment - Ui Material  step2 (NavBar)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NavBar from "./components/NavBar/NavBar";
+/*import NavBar from "./components/NavBar/NavBar";*/
 import DefaultThemeProvider from "./components/themes/DefaultThemeProvider";
 import CssBaseline from "@material-ui/core/CssBaseline";
+/*import 'bootstrap/dist/css/bootstrap.min.css';*/
+import '../css/app.css';
+import Grid from "@material-ui/core/Grid";
+import {makeStyles} from "@material-ui/styles";
+import NavBar from "./components/NavBar/NavBar";
 
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
 
 export default function App() {
-
-
+    const classes = useStyles();
     return (
-        <NavBar/>
+        <div className={classes.root}>
+            <Grid container spacing={3}>
+                <Grid item xs={12}>
+                    <NavBar/>
+                </Grid>
+
+            </Grid>
+        </div>
+
+
     );
 };
 
 ReactDOM.render(
     <DefaultThemeProvider>
-    <CssBaseline/>
-    <App/>
-    </DefaultThemeProvider>
-
-    , document.getElementById('root'));
+        <CssBaseline/>
+        <App/>
+    </DefaultThemeProvider>, document.getElementById('root'));
 
--- assets/js/components/NavBar/NavBar.js
+++ assets/js/components/NavBar/NavBar.js
@@ -1,13 +1,88 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
+import './Navbar.css';
+import {
+    AppBar,
+    Box,
+    Button,
+    Drawer,
+    IconButton,
+    Link,
+    List,
+    ListItem,
+    ListItemText,
+    makeStyles,
+    Toolbar
+} from '@material-ui/core';
+import {List as ListIcon, Menu as MenuIcon} from '@material-ui/icons';
+import AccountBoxIcon from '@material-ui/icons/SentimentVerySatisfied';
+import BuildIcon from '@material-ui/icons/Build';
+import ImportantDevicesIcon from '@material-ui/icons/ImportantDevices';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ContactPhoneIcon from '@material-ui/icons/ContactPhone';
+import Divider from "@material-ui/core/Divider";
 
+const useStyles = makeStyles(style => ({
+    menuIcon: {
+        marginRight: style.spacing(2),
+    },
+    list: {
+        width: '200px',
+        height: '30px'
+    },
+    menu: {
+        marginLeft: '20px',
+        borderBottom: 'white'
+    }
+
+}))
 
 const NavBar = () => {
+    //styles
+    const classes = useStyles();
+    //state
+    const [drawerOpen, setDrawerOpen] = useState(false);
+    //functions
+    const toogleDrawer = () => {
+        setDrawerOpen(!drawerOpen)
+    };
 
+    const draweritems = [
+        {text: 'ACCUEIL', icon: <ListIcon fontSize="small"/>, divider: <Divider/>},
+        {text: 'A PROPOS', icon: <AccountBoxIcon fontSize="small"/>},
+        {text: 'SERVICES', icon: <BuildIcon fontSize="small"/>},
+        {text: 'PORTOFOLIO', icon: <ImportantDevicesIcon fontSize="small"/>},
+        {text: 'CLIENTS', icon: <ThumbUpAltIcon fontSize="small"/>},
+        {text: 'CONTACT', icon: <ContactPhoneIcon fontSize="small"/>}
+    ];
+    const divider = [
+        {divider: <Divider/>}
+    ]
 
     return (
-        <div>
-            <h1>Hello</h1>
-        </div>
+        <AppBar position="fixed">
+            <Toolbar>
+                <IconButton onClick={toogleDrawer}
+                            className={classes.menuIcon}
+                            edge="start"><MenuIcon/></IconButton>
+                <Link href="/" underline="none" color="inherit" variant="h6">EUPHONY</Link>
+                <Box flexGrow={1}/>
+                <Button size="large">Login</Button>
+                <Drawer anchor="left" variant="temporary" onClose={toogleDrawer} open={drawerOpen}>
+                    <List className={classes.list}>
+                        {draweritems.map(prop => (
+                            <ListItem onClick={toogleDrawer} button key={prop.text}>
+                                <Fragment>{prop.icon} </Fragment>
+                                <ListItemText className={classes.menu}>{prop.text}</ListItemText>
+                                {prop.divider}
+                            </ListItem>
+                        ))}
+                    </List>
+
+                </Drawer>
+
+
+            </Toolbar>
+        </AppBar>
     );
 };
 
